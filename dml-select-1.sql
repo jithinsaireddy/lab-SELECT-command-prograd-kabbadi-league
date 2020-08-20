a550290500d4203ed140c2972b205ca4840064f3
jithinsaireddy <jithinsai2014@gmail.com>
2020-08-20 13:21:06 +0530

    Done

--- dml-select-1.sql
+++ dml-select-1.sql
@@ -40,3 +40,119 @@
 -- 19. **Write a query to display the names of the Winning teams in the month of March 2020. Display the records sorted in ascending order based on team name.**
 
 -- 20. **Write a query to display the names of players who were the player of the match in the games held in March 2020. Display the records sorted in ascending order based on player name.**
+
+SELECT name FROM team
+ORDER BY name ;
+
+
+SELECT name FROM player 
+WHERE name LIKE 's%' AND country!='South Africa'
+ORDER BY name DESC;
+
+SELECT event_no,clock_in_seconds FROM event
+ORDER BY event_no ASC; 
+
+SELECT event_no FROM event
+WHERE raid_points + defending_points >= 3
+ORDER BY event_no ; 
+
+SELECT name FROM player
+WHERE (country = 'India' OR country = 'Iran')
+ORDER BY name ASC;
+
+SELECT name FROM player
+WHERE (country != 'India' AND country != 'Iran')
+ORDER BY name ASC; 
+
+
+SELECT name FROM skill
+WHERE name LIKE 'Defend%'
+ORDER BY name ASC;
+
+
+SELECT stadium_name FROM venue
+WHERE stadium_name LIKE '%Complex'
+ORDER BY stadium_name ASC;
+
+SELECT id FROM outcome
+WHERE score < 35
+ORDER BY id DESC;
+
+
+SELECT game_date FROM game
+WHERE game_date BETWEEN TO_DATE('01-JAN-2020', 'DD-MM-YYYY') AND '31-MAR-2020'
+ORDER BY game_date DESC;
+
+
+SELECT name FROM player
+WHERE skill_id > 1
+ORDER BY name ASC;
+
+
+SELECT event_no,raid_points,defending_points FROM event,game
+WHERE innings_id = 1 AND game_date = TO_DATE('26-JAN-2020', 'DD-MM-YYYY')
+ORDER BY event_no ASC;
+
+SELECT winner_team_id FROM outcome,game,team
+WHERE game_date = TO_DATE('09-MAR-2020', 'DD-MM-YYYY')
+ORDER BY name ASC;
+
+SELECT name FROM game,player,outcome
+WHERE game_date = TO_DATE('09-MAR-2020', 'DD-MM-YYYY')
+ORDER BY player_of_match ASC;
+
+
+SELECT name FROM player
+WHERE skill_id=3
+ORDER BY name ASC;
+
+SELECT stadium_name FROM game,venue
+WHERE game_date = TO_DATE('09-MAR-2020', 'DD-MM-YYYY')
+ORDER BY game.venue_id ASC;
+
+SELECT coach FROM team,player
+WHERE country = 'Iran'
+ORDER BY team.name;
+
+
+select event_no,defending_points FROM event,player
+WHERE player.name = 'Fazel Atrachali'
+ORDER BY event.event_no;
+
+SELECT name FROM team,outcome,game
+WHERE game_date BETWEEN TO_DATE('01-MAR-2020', 'DD-MM-YYYY') AND '31-MAR-2020'
+ORDER BY team.name;
+
+SELECT name FROM player,outcome,game
+WHERE game_date BETWEEN TO_DATE('01-MAR-2020', 'DD-MM-YYYY') AND '31-MAR-2020'
+ORDER BY player.name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
